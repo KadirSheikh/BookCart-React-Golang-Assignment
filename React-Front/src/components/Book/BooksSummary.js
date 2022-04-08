3be5c01b64ebf9d0127bcbd5b5b3fec22e2609a7
KadirSheikh <rs7246510@gmail.com>
2022-04-08 11:13:13 +0530

    Edited book summery text

--- React-Front/src/components/Book/BooksSummary.js
+++ React-Front/src/components/Book/BooksSummary.js
@@ -4,10 +4,7 @@ const BooksSummary = () => {
   return (
     <section className={classes.summary}>
       <h2>Expand your mind, read a book.</h2>
-      <p>
-        Choose your favorite book from our broad selection of available books
-        and enjoy a reading.
-      </p>
+      <p>Browse your favorite book from broad selection of available books.</p>
     </section>
   );
 };
